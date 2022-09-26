a59379f7e71a8df0c7311fd7e9844bc1a5b40200
Gabriela Quadros <ngabiquadros@gmail.com>
2022-09-25 22:10:29 -0300

    confirmação de senha

--- js/login.js
+++ js/login.js
@@ -1,20 +1,23 @@
 //criar conta
 document.getElementById("create-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Anote sua senha em local seguro, não será possivel recupera-la!");
 
   const email = document.getElementById("user").value;
   const password = document.getElementById("password").value;
+  const passwordconfirm = document.getElementById("passwordrepeat").value;
   console.log(email, password);
-  // if (password !== password) {
-  //   alert("As senhas não coincidem! Tente novamente!");
-  // return;
-  // }
+
+  if (passwordconfirm !== password) {
+    alert("As senhas não coincidem! Tente novamente!");
+    return;
+  }
+
   saveAccount({
     login: email,
     password: password,
     recados: [],
   });
+  alert("Anote sua senha em local seguro, não será possivel recupera-la!");
   alert("Conta criada com sucesso!");
   window.location.href = "index.html";
 });
